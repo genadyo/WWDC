7a1c50b395ae459a70220dcc1a5e0ae74220a805
Genady Okrain <genady@okrain.com>
2014-11-20 16:09:19 +0200

    Fixed #25

--- Podfile
+++ Podfile
@@ -1,5 +1,6 @@
 platform :ios, '7.0'
 inhibit_all_warnings!
 
+pod 'TUSafariActivity'
 pod 'GoogleAnalytics-iOS-SDK'
 pod 'TMCache'
--- WWDC/WDCPartyWebViewController.swift
+++ WWDC/WDCPartyWebViewController.swift
@@ -10,24 +10,46 @@ import UIKit
 import WebKit
 
 @objc class WDCPartyWebViewController: UIViewController {
-    var webView: WKWebView?
-    var url = NSURL?()
+    var url: NSURL?
+    var observer: JVObserver?
+
+    let webView = WKWebView()
+    let progressView = UIProgressView(progressViewStyle: .Default)
 
     override func loadView() {
         super.loadView()
 
-        self.webView = WKWebView()
+        // load progress and web views
+        webView.addSubview(progressView)
         self.view = webView
+
+        // auto layout progress view
+        progressView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        let views = ["progressView": self.progressView]
+        webView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[progressView]|", options: nil, metrics: nil, views: views))
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        webView!.loadRequest(NSURLRequest(URL:url!))
+        // load url
+        webView.loadRequest(NSURLRequest(URL:url!))
+
+        // update progress
+        observer = JVObserver(forObject: webView, keyPath: "estimatedProgress", target: self) { [weak self] (AnyObject) -> Void in
+            self?.progressView.progress = Float((self?.webView.estimatedProgress)!)
+            if (self?.webView.estimatedProgress == 1.0) {
+                UIView.animateWithDuration(0.2) {
+                    self?.progressView.alpha = 0
+                    return
+                }
+            }
+        }
     }
 
     @IBAction func share(sender: AnyObject) {
-        let activityViewController : UIActivityViewController = UIActivityViewController(activityItems: [title!, url!], applicationActivities: nil)
+        let activity = TUSafariActivity() // open in safari
+        let activityViewController : UIActivityViewController = UIActivityViewController(activityItems: [title!, url!], applicationActivities: [activity])
         self.presentViewController(activityViewController, animated: true, completion: nil)
     }
 }
