1a7a5355380cc5ccd43af09941b24dc565c6eec7
Genady Okrain <genady@okrain.com>
2014-11-22 15:20:46 +0200

    resize cached watch icons

--- SFPartiesKit/WDCParty.m
+++ SFPartiesKit/WDCParty.m
@@ -227,9 +227,38 @@
 
 - (void)cacheWatchIcon:(UIImage *)icon
 {
+    UIImage *resizeIcon = [WDCParty imageWithImage:icon scaledToMaxWidth:35.0f maxHeight:35.0f];
     NSUserDefaults *userDefaults = [[NSUserDefaults alloc] initWithSuiteName:@"group.so.sugar.SFParties"];
-    [userDefaults setObject:UIImagePNGRepresentation(icon) forKey:[self iconCacheKey]];
+    [userDefaults setObject:UIImagePNGRepresentation(resizeIcon) forKey:[self iconCacheKey]];
     [userDefaults synchronize];
+}
+
++ (UIImage *)imageWithImage:(UIImage *)image scaledToSize:(CGSize)size
+{
+    if ([[UIScreen mainScreen] respondsToSelector:@selector(scale)]) {
+        UIGraphicsBeginImageContextWithOptions(size, NO, [[UIScreen mainScreen] scale]);
+    } else {
+        UIGraphicsBeginImageContext(size);
+    }
+    [image drawInRect:CGRectMake(0, 0, size.width, size.height)];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+
+    return newImage;
+}
+
++ (UIImage *)imageWithImage:(UIImage *)image scaledToMaxWidth:(CGFloat)width maxHeight:(CGFloat)height
+{
+    CGFloat oldWidth = image.size.width;
+    CGFloat oldHeight = image.size.height;
+
+    CGFloat scaleFactor = (oldWidth > oldHeight) ? width / oldWidth : height / oldHeight;
+
+    CGFloat newHeight = oldHeight * scaleFactor;
+    CGFloat newWidth = oldWidth * scaleFactor;
+    CGSize newSize = CGSizeMake(newWidth, newHeight);
+
+    return [self imageWithImage:image scaledToSize:newSize];
 }
 
 @end
