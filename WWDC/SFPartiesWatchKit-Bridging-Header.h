30df18bad950e0505c5ac334cb6cb55d017cdb49
Genady Okrain <genady@okrain.com>
2014-11-22 17:00:47 +0200

    fix the h headers

--- WWDC/SFPartiesWatchKit-Bridging-Header.h
+++ WWDC/SFPartiesWatchKit-Bridging-Header.h
@@ -2,5 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "WDCParty.h"
-#import "WDCParties.h"
+#import "../SFPartiesKit/WDCParty.h"
+#import "../SFPartiesKit/WDCParties.h"
