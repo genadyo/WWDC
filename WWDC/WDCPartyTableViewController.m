a8e8678d13fb916a123ff500203ee74207e574f1
Genady Okrain <genady@okrain.com>
2015-05-21 23:38:57 +0300

    Fixed #58

--- WWDC/WDCPartyTableViewController.m
+++ WWDC/WDCPartyTableViewController.m
@@ -132,13 +132,11 @@
 - (void)refreshGoing
 {
     if ([[SDCloudUserDefaults objectForKey:@"going"] indexOfObject:self.party.objectId] == NSNotFound) {
-        [self.goingButton setTitle:NSLocalizedString(@"Not Going", nil) forState:UIControlStateNormal];
         [self.goingButton setTitleColor:[UIColor colorWithRed:106.0/255.0f green:118.0/255.f blue:220.f/255.0f alpha:1.0f] forState:UIControlStateNormal];
         [self.goingButton setTitleColor:[UIColor colorWithRed:106.0/255.0f green:118.0/255.f blue:220.f/255.0f alpha:0.3f] forState:UIControlStateHighlighted];
-        [self.goingButton setImage:nil forState:UIControlStateNormal];
-        [self.goingButton setImage:nil forState:UIControlStateHighlighted];
+        [self.goingButton setImage:[Assets imageOfTogglegoingWithInitColor:[UIColor colorWithRed:0.0f/255.0f green:0.0f/255.0f blue:0.0f/255.0f alpha:0.0f]] forState:UIControlStateNormal];
+        [self.goingButton setImage:[Assets imageOfTogglegoingWithInitColor:[UIColor colorWithRed:0.0f/255.0f green:0.0f/255.0f blue:0.0f/255.0f alpha:0.0f]] forState:UIControlStateHighlighted];
     } else {
-        [self.goingButton setTitle:NSLocalizedString(@"Going", nil) forState:UIControlStateNormal];
         [self.goingButton setTitleColor:[UIColor colorWithRed:46.0f/255.0f green:204.0/255.f blue:113.f/255.0f alpha:1.0f] forState:UIControlStateNormal];
         [self.goingButton setTitleColor:[UIColor colorWithRed:46.0f/255.0f green:204.0/255.f blue:113.f/255.0f alpha:0.3f] forState:UIControlStateHighlighted];
         [self.goingButton setImage:[Assets imageOfTogglegoingWithInitColor:[UIColor colorWithRed:46.0f/255.0f green:204.0f/255.0f blue:113.0f/255.0f alpha:1.0f]] forState:UIControlStateNormal];
